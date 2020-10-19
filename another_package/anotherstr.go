7f267817b339a8dd6d3d836792a1e00611ef639f
YuvalAvra <yuval.avrahami@gmail.com>
2020-10-19 02:43:17 -0700

    add the another_package subpacakge

--- another_package/anotherstr.go
+++ another_package/anotherstr.go
@@ -1,11 +1,9 @@
 // Package morestrings implements additional functions to manipulate UTF-8
 // encoded strings, beyond what is provided in the standard "strings" package.
-package evenmorestrings
-
-import "github.com/yuvalavra/evenmorestrings/another_package"
+package another_package
 
 // ReverseRunes returns its argument string reversed rune-wise left to right.
-func DoubleString(s string) string {
+func TripleString(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
@@ -13,11 +11,9 @@ func DoubleString(s string) string {
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
 	return ((string)(r))
-}
-
-
-func CallAnotherPackTripleStr(s string) string {
-    return DoubleString(another_package.TripleString(s))
 }
 
